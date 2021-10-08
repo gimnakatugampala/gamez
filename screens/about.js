fdc694b65236d9f307cdd3569d15ff7a3970e218
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-10-08 14:03:29 -0700

    screens

---
+++ screens/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function about() {
+    return (
+        <View>
+            <Text>About Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
---
+++ screens/reviewDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function reviewDetails() {
+    return (
+        <View style={styles.container}>
+            <Text>review Details</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        padding:24
+    }
+})
